9a7f31909357a953da14b88efd0d6f4749c764f8
TobiasReaper47 <tobiasreaper4766@gmail.com>
2025-01-03 09:17:55 +0000

    02/Deklaration

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -3,3 +3,17 @@
 /**
  * Blockkommentar
  */
+
+/****** Variablen 01 *****/
+
+console.log("Hi!");
+//console.log(Hi);
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; //Wertzuweisung | assignment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; //Deklaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " + familyName + "!");
