9569a6849158cc076209df7ce8109f6dbf45709a
TobiasReaper47 <tobiasreaper4766@gmail.com>
2025-01-03 11:26:52 +0000

    03/prompt test

---
+++ src/03_prompt.js
@@ -0,0 +1,12 @@
+// Installation:
+// npm install -g npm@latest
+// npm cache clean --force
+// npm install -g prompt sync
+
+
+const prompt = require("prompt-sync")({sigint: true});
+
+// Alles muss man selber machen lassen! ...
+let firstName = prompt("Vorname?:");
+let familyName = prompt("Name?:")
+console.log("Hallo, " + firstName + " " + familyName +"!");
