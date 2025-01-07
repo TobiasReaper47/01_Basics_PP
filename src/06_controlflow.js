234bf57f000d70d1c3946e74892194e9b8b5d556
TobiasReaper47 <tobiasreaper4766@gmail.com>
2025-01-07 11:41:09 +0000

    06/Switch Case2

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -67,42 +67,59 @@ isJohnEqual = (ageJohn == ageMark);
 
 /****** Fallunterscheidung / SWITCH|CASE 1 ******/
 
-const firstName = "Jane";
-let job;
+// const firstName = "Jane";
+// let job;
 
-job = "driver";  // .. fährt TAXI! / UBER
-job = "diver"; // .. taucht im Rhein! 
-job = "artist"; // .. malt ein Bild!
-job = "pilot"; // .. macht etwas anderes! --> default
-job = "teacher"; // .. unterrichtet!
-job = "instructor"; // .. unterrichtet!
-job = "hunter"; // ..jagt!
+// job = "driver";  // .. fährt TAXI! / UBER
+// job = "diver"; // .. taucht im Rhein! 
+// job = "artist"; // .. malt ein Bild!
+// job = "pilot"; // .. macht etwas anderes! --> default
+// job = "teacher"; // .. unterrichtet!
+// job = "instructor"; // .. unterrichtet!
+// job = "hunter"; // ..jagt!
 
 
-switch (job) 
-{
-    case "driver": // job == "driver"
-        console.log(firstName + " fährt TAXI!");
-        break;
-    case "diver": // job == "diver"
-        console.log(firstName + " taucht im Rhein!");
-        break;
-    case "artist": // job == "artist"
-        console.log(firstName + " malt ein Bild!");
-        break;
-    case "teacher": // job == "teacher"
-    case "instructor": // job =="instructor"    
-        console.log(firstName + " unterrichtet!");
-        break;
-    case "hunter": // job == "hunter"
-        console.log(firstName + " jagt!");
-        break;
+// switch (job) 
+// {
+//     case "driver": // job == "driver"
+//         console.log(firstName + " fährt TAXI!");
+//         break;
+//     case "diver": // job == "diver"
+//         console.log(firstName + " taucht im Rhein!");
+//         break;
+//     case "artist": // job == "artist"
+//         console.log(firstName + " malt ein Bild!");
+//         break;
+//     case "teacher": // job == "teacher"
+//     case "instructor": // job =="instructor"    
+//         console.log(firstName + " unterrichtet!");
+//         break;
+//     case "hunter": // job == "hunter"
+//         console.log(firstName + " jagt!");
+//         break;
         
         
 
 
 
-    default: //WICHTIG!!
-        console.log(firstName + " macht etwas anderes!");
+//     default: //WICHTIG!!
+//         console.log(firstName + " macht etwas anderes!");
+//         break;
+// }
+
+/********  Fallunterscheidung / SWITCH|CASE 2 *******/
+
+const cond = true;
+const value = 2;
+
+switch (cond) {
+    case (value == 1):
+        console.log("Value hat den Wert 1.");
+        break;
+    case (value == 2):
+        console.log("Value hat den Wert 2.");
+        break;
+    default:
+        console.log("Value hat einen anderen Wert.");
         break;
 }
