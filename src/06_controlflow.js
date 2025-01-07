dbec58334590a7f945ee9c589fab397c8725700a
TobiasReaper47 <tobiasreaper4766@gmail.com>
2025-01-07 10:14:35 +0000

    06/Ternary Operator

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -1,7 +1,7 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assignment
-const ageJohn = 32;
+const ageJohn = 30;
 const ageMark = 30;
 
 // Deklaration
@@ -38,9 +38,12 @@ console.log("------------------");
 // if (true)
 // if (false)
 // if (isJohnOlder)
-if(ageJohn > ageMark)
-{
-    console.log("John ist älter!");  
-} else {
-    console.log("John ist jünger!");    
-}
+// if(ageJohn > ageMark)
+// {
+//     console.log("John ist älter!");  
+// } else {
+//     console.log("John ist jünger!");    
+// }
+
+/************  Ternäre (ternary) Schreibweise ************/
+console.log( (isJohnOlder) ? "John is older" : "Mark is older");
