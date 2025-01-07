db2dd061f686618c40c2feefd8bf518ef8972801
TobiasReaper47 <tobiasreaper4766@gmail.com>
2025-01-07 08:22:34 +0000

    05_test

---
+++ src/05_wahrheitswerte.js
@@ -0,0 +1,24 @@
+
+/* Berechnung Alter */
+
+// Deklaration
+let ageJohn, ageMark;
+const birthYearJohn = 1980;
+const birthYearMark = 1990;
+
+// Berechnung Alter
+let date = new Date();
+let year = date.getFullYear();
+ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+// console.log("Datum. " + date);
+
+// Tests
+let isJohnOlder; // Is John older? --> y/n --> true | false
+isJohnOlder = (ageJohn > ageMark);
+
+console.log("isJohnOlder: " + isJohnOlder);
