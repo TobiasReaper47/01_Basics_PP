6829e2aab765b3e44ce34bfaf2afd6e1a74a4a64
TobiasReaper47 <tobiasreaper4766@gmail.com>
2025-01-08 08:09:24 +0000

    08/1.Kapselung von Codeblöcken

---
+++ src/08_funktion_1.js
@@ -0,0 +1,12 @@
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf | call
+test();
+
+// Funktionsrumpf | callee
+// Funktionsdelaration
+function test()
+{
+console.log("Hallo Karl!");
+}
