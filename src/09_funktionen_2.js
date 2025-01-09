c448ddbadc745a7db67394f8a20e6124c2ceb217
TobiasReaper47 <tobiasreaper4766@gmail.com>
2025-01-09 08:03:20 +0000

    09/module output

---
+++ src/09_funktionen_2.js
@@ -0,0 +1,20 @@
+/*** Rechner */
+/*
+0. a+b | a-b | a*b | a/b  --> Ergebnis c 
+
+1. Dateneingabe + -überprüfung :  
+2. Auswahl Rechenart : 
+3. Fkt. Grundrechenarten : 
+4. Ausgabe in Konsole : check!
+*/
+
+ 
+
+//module: output | test :
+// output("hi");
+// output(2);
+// output(true);
+function output(outputData) 
+{
+    console.log(outputData);
+}
