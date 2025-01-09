373f40b8ad2cac8a9ae0db2ccf7905d3b6b71e04
TobiasReaper47 <tobiasreaper4766@gmail.com>
2025-01-09 09:18:48 +0000

    09/division 1. Iteration

--- src/09_funktionen_2.js
+++ src/09_funktionen_2.js
@@ -8,6 +8,19 @@
 4. Ausgabe in Konsole : check!
 */
 
+// module: division a / b |  test:
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+// output(divide(0,2));
+// output(divide(3,0));
+// output(divide(0,0));
+function divide(a,b) 
+{
+    return a / b
+}
+
+
 // module: multiplication a * b |  test:
 // output(multiply(3,2));
 // output(multiply(3,-2));
